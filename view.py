2b231a1b860330630c37dfb56bc1e38e744a3cf4
Lasse Foster <lasse.foster@web.de>
2024-11-18 12:53:34 +0100

    * prettier printing for view.py

--- view.py
+++ view.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from keras import saving, ops
+import FaceIdentifier
 
 
 # Function to load a specified number of random images with true labels
@@ -59,16 +60,17 @@ def show_random_predictions(images, true_labels, save_path='predictions.png'):
         true_face, true_age, true_gender = true_labels[i]
 
         # Extract single values from the predictions
-        pred_face = float(ops.sigmoid(predictions[0][0]))  # Access the first element to get the scalar
-        pred_age = float(ops.relu(predictions[1]))  # Access the first element for the scalar
-        pred_gender = float(ops.argmax(predictions[2]))  # Argmax works directly as expected
+        pred_face = float(predictions['face_output'][i])  # Access the first element to get the scalar
+        pred_age = round(predictions['age_output'][i][0])  # Access the first element for the scalar
+        pred_gender = float(ops.argmax(predictions['gender_output'][i]))  # Argmax works directly as expected
 
         # Display the image with true vs predicted labels
         plt.subplot(2, 3, i + 1)
         plt.imshow(image.astype("uint8"))
         plt.axis('off')
-        plt.title(f"True: Face {true_face}, Age {true_age}, Gender {true_gender}\n"
-                  f"Pred: Face {int(pred_face)}, Age {int(pred_age)}, Gender {pred_gender}")
+        title_true = f"True: Face 100%, Age {true_age}, Gender {"male" if true_gender == 0 else "female" }\n" if true_face == 1 else "True: Face 0%\n"
+        title_pred = f"Pred: Face {100 * pred_face:.2f}%, Age {pred_age}, Gender {"male" if pred_gender == 0 else "female" }" if pred_face >= 0.5 else f"Pred: Face {100 * pred_face:.2f}%"
+        plt.title(title_true + title_pred)
 
     plt.tight_layout()
     # Save plot to file instead of showing it
